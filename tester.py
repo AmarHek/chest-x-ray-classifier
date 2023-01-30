1fc69d433fe1d71652a47a98b50271ac6cf40798
Amar <amar.hekalo@uni-wuerzburg.de>
2023-01-30 17:52:39 +0100

    Set up file for tester class

---
+++ tester.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+from dataset import CheXpert
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+import libauc
+
+
+class Tester:
+    # TODO
+    pass
