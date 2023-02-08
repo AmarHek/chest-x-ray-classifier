b0bfe69e339d1e11c3c72ed935321a9ee7f9c81b
Amar <amar.hekalo@uni-wuerzburg.de>
2023-02-08 15:15:09 +0100

    some tests

--- Scripts/tests.py
+++ Scripts/tests.py
@@ -11,20 +11,21 @@ if __name__ == '__main__':
                "Pneumonia","Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other","Fracture","Support Devices"]
 
     trainSet = CheXpert(csv_path=csv_path + 'train.csv', image_root_path=img_path, use_upsampling=False,
-                        use_frontal=True, image_size=10, mode='train', train_cols=classes)
+                        use_frontal=True, image_size=100, mode='train', train_cols=classes)
     valSet = CheXpert(csv_path=csv_path + 'valid.csv', image_root_path=img_path, use_upsampling=False,
-                      use_frontal=True, image_size=10, mode='valid', train_cols=classes)
+                      use_frontal=True, image_size=100, mode='valid', train_cols=classes)
 
     model = models.sequential_model("densenet121", trainSet.num_classes)
 
     trainer = Trainer(model=model,
-                      train_set=trainSet,
+                      train_set=valSet,
                       valid_set=valSet,
                       loss="bce",
                       optimizer="adam",
                       learning_rate=0.01,
                       epochs=100,
-                      batch_size=1000,
+                      batch_size=50,
+                      update_steps=1,
                       early_stopping_patience=10,
                       lr_scheduler="reduce",
                       plateau_patience=5)
