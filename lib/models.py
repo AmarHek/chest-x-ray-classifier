32c03b483154c335c830aba982ef4dfbf70581ef
Amar <amar.hekalo@uni-wuerzburg.de>
2023-02-08 13:46:23 +0100

    added dict for model_output_shape to avoid using LazyLinear

--- lib/models.py
+++ lib/models.py
@@ -64,6 +64,43 @@ def get_classifier_function(classifier_function: str):
     return classifier_functions[classifier_function]
 
 
+def get_classifier(architecture: str, n_classes: int, classifier_function,
+                   long_classifier: bool = False, dropout: float = 0.2) -> nn.Module:
+
+    shape_dict = {
+        "densenet121": 1024,
+        "densenet161": 2208,
+        "efficientnet_b1": 1280,
+        "efficientnet_b2": 1408,
+        "efficientnet_v2_s": 1280,
+        "efficientnet_v2_m": 1280,
+        "efficientnet_v2_l": 1280,
+        "resnet50": 2048,
+        "resnet101": 2048,
+        "resnet152": 2048,
+        "resnext50_32x4d": 2048,
+        "resnext101_32x8d": 2048,
+        "resnext101_64x4d": 2048,
+        "inception_v3": 2048
+    }
+
+    assert architecture in shape_dict.keys(), "Invalid architecture!"
+    out_features = shape_dict[architecture]
+
+    # define a new classifier with custom layers
+    if long_classifier:
+        classifier = nn.Sequential(nn.Linear(out_features, 512),
+                                   nn.ReLU(),
+                                   nn.Dropout(dropout),
+                                   nn.Linear(512, n_classes),
+                                   classifier_function)
+    else:
+        classifier = nn.Sequential(nn.Linear(out_features, n_classes),
+                                   classifier_function)
+
+    return classifier
+
+
 def sequential_model(architecture: str, n_classes: int, pretrained: bool = True, finetune: bool = False,
                      long_classifier: bool = False, classifier_function: str = "sigmoid",
                      dropout: float = 0.2) -> nn.Module:
@@ -77,16 +114,8 @@ def sequential_model(architecture: str, n_classes: int, pretrained: bool = True,
         for param in model.parameters():
             param.requires_grad = False
 
-    # define a new classifier with custom layers
-    if long_classifier:
-        classifier = nn.Sequential(nn.LazyLinear(512),
-                                   nn.ReLU(),
-                                   nn.Dropout(dropout),
-                                   nn.Linear(512, n_classes),
-                                   classifier_function)
-    else:
-        classifier = nn.Sequential(nn.LazyLinear(n_classes),
-                                   classifier_function)
+    classifier = get_classifier(architecture, n_classes, classifier_function=classifier_function,
+                                long_classifier=long_classifier, dropout=dropout)
 
     # replace the pretrained model's classifier with our new one
     if "resnet" in architecture or "inception" in architecture:
@@ -98,5 +127,5 @@ def sequential_model(architecture: str, n_classes: int, pretrained: bool = True,
 
 
 if __name__ == "__main__":
-    model = sequential_model("inception_v3", 5, classifier_function="sigmoid")
-    print(model)
+    test_model = sequential_model("inception_v3", 5, classifier_function="sigmoid")
+    print(test_model)
