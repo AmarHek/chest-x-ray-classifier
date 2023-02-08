3d1efc6668c069e92f92993cc17780ac20e045c9
Amar <amar.hekalo@uni-wuerzburg.de>
2023-02-08 12:33:23 +0100

    moved test.py to scripts

--- Scripts/tests.py
+++ Scripts/tests.py
@@ -1,19 +1,19 @@
-from dataset import CheXpert
-from trainer import Trainer
-import models
+from lib.dataset import CheXpert
+from lib.trainer import Trainer
+from lib import models
 
 if __name__ == '__main__':
 
-    csv_path = '\\\\hastur\\scratch\\hekalo\\Datasets\\CheXpert-v1.0-small\\'
-    img_path = '\\\\hastur\\scratch\\hekalo\\Datasets\\'
+    csv_path = '//hastur/scratch/hekalo/Datasets/CheXpert-v1.0-small/'
+    img_path = '//hastur/scratch/hekalo/Datasets/'
 
-    classes = ["Enlarged Cardiomediastinum","Cardiomegaly","Lung Opacity","Lung Lesion","Edema","Consolidation","Pneumonia",
-               "Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other","Fracture","Support Devices"]
+    classes = ["Enlarged Cardiomediastinum","Cardiomegaly","Lung Opacity","Lung Lesion","Edema","Consolidation",
+               "Pneumonia","Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other","Fracture","Support Devices"]
 
     trainSet = CheXpert(csv_path=csv_path + 'train.csv', image_root_path=img_path, use_upsampling=False,
-                        use_frontal=True, image_size=320, mode='train', train_cols=classes)
+                        use_frontal=True, image_size=10, mode='train', train_cols=classes)
     valSet = CheXpert(csv_path=csv_path + 'valid.csv', image_root_path=img_path, use_upsampling=False,
-                      use_frontal=True, image_size=320, mode='valid', train_cols=classes)
+                      use_frontal=True, image_size=10, mode='valid', train_cols=classes)
 
     model = models.sequential_model("densenet121", trainSet.num_classes)
 
@@ -24,7 +24,7 @@ if __name__ == '__main__':
                       optimizer="adam",
                       learning_rate=0.01,
                       epochs=100,
-                      batch_size=2,
+                      batch_size=1000,
                       early_stopping_patience=10,
                       lr_scheduler="reduce",
                       plateau_patience=5)
