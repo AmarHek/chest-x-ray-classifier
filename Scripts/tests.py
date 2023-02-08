af0dd83d5d838ac08ea47ade240fd103b6481d68
Amar <amar.hekalo@uni-wuerzburg.de>
2023-02-08 16:28:10 +0100

    more testing

--- Scripts/tests.py
+++ Scripts/tests.py
@@ -4,30 +4,30 @@ from lib import models
 
 if __name__ == '__main__':
 
-    csv_path = '//hastur/scratch/hekalo/Datasets/CheXpert-v1.0-small/'
-    img_path = '//hastur/scratch/hekalo/Datasets/'
+    csv_path = 'C:/Users/Amar/Documents/CheXpert-v1.0-small/'
+    img_path = 'C:/Users/Amar/Documents/'
 
     classes = ["Enlarged Cardiomediastinum","Cardiomegaly","Lung Opacity","Lung Lesion","Edema","Consolidation",
                "Pneumonia","Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other","Fracture","Support Devices"]
 
     trainSet = CheXpert(csv_path=csv_path + 'train.csv', image_root_path=img_path, use_upsampling=False,
-                        use_frontal=True, image_size=100, mode='train', train_cols=classes)
+                        use_frontal=True, image_size=320, mode='train', train_cols=classes)
     valSet = CheXpert(csv_path=csv_path + 'valid.csv', image_root_path=img_path, use_upsampling=False,
-                      use_frontal=True, image_size=100, mode='valid', train_cols=classes)
+                      use_frontal=True, image_size=320, mode='valid', train_cols=classes)
 
     model = models.sequential_model("densenet121", trainSet.num_classes)
 
     trainer = Trainer(model=model,
-                      train_set=valSet,
+                      train_set=trainSet,
                       valid_set=valSet,
                       loss="bce",
                       optimizer="adam",
                       learning_rate=0.01,
                       epochs=100,
-                      batch_size=50,
+                      batch_size=8,
                       update_steps=1,
                       early_stopping_patience=10,
-                      lr_scheduler="reduce",
+                      lr_scheduler="plateau",
                       plateau_patience=5)
 
     model_path = "//hastur/scratch/hekalo/Models/test/"
