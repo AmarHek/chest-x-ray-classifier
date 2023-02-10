438ff13612c023c34320430c2cb883485ed08a3c
Amar <amar.hekalo@uni-wuerzburg.de>
2023-02-10 15:17:26 +0100

    sbatch script to run many many experiments

---
+++ Scripts/bce.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Activate the virtual environment
+source /autofs/ls6/hekalo/Git/chest-x-ray-classifier/venv/bin/activate
+
+# add project root to PYTHONPATH
+PYTHONPATH="${PYTHONPATH}:/autofs/ls6/hekalo/Git/chest-x-ray-classifier"
+export PYTHONPATH
+
+# Define model names
+architectures=(
+  "densenet121"
+  "efficientnet_b1"
+  "efficientnet_v2_s"
+  "resnet50"
+  "efficientnet_b2"
+  "resnet101"
+  "resnet152"
+  "efficientnet_v2_m"
+  "densenet161"
+  "efficientnet_v2_l"
+  "resnext101_32x4d"
+  "resnext101_64x4d"
+  "inception_v3"
+)
+
+classes_sets=(
+  "pneumonia"
+  "chexternal"
+  "chexternal_pneumo"
+  "chexpert"
+)
+
+model_path = "/scratch/hekalo/Models/labels_chexpert/bce/"
+
+csv_path="/scratch/hekalo/Datasets/CheXpert-v1.0-small/"
+img_path="/scratch/hekalo/Datasets/"
+image_size=320
+loss="bce"
+optimizer="adam"
+learning_rate=0.01
+batch_size=32
+epochs=100
+
+# Iterate through model names
+for architecture in "${architectures[@]}"; do
+  # Iterate through classes
+  for classes in "${classes_sets[@]}"; do
+    # Submit the script as a SLURM job with the current combination of model_name_or_path and dataset_names_or_paths
+    sbatch -p ls6 --gres=gpu:rtx2080ti:1 --wrap="python train1.py --architecture=$architecture --classes=$classes
+    --model_path=$model_path$classes --csv_path=$csv_path --img_path=$img_path --image_size=$image_size
+    --loss=$loss --optimizer=$optimizer --learning_rate=$learning_rate  --batch_size=$batch_size
+    --epochs=$epochs --lr_scheduler='reduce' --es_patience=5"
+  done
+done
+
+# Deactivate the virtual environment
+deactivate
