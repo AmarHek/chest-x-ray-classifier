b72b230b5e70b2f4ae4324ddd41ff60b0ab19a86
Amar <amar.hekalo@uni-wuerzburg.de>
2023-03-14 14:43:34 +0100

    added methods load_mdoel and add_model to tester class

--- lib/tester.py
+++ lib/tester.py
@@ -1,11 +1,30 @@
+import os.path
+
 import torch.nn as nn
-from .dataset import CheXpert
+import torch.cuda
 from torch.utils.data import DataLoader
-import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
 import libauc
+from tqdm import tqdm
+
+from lib.dataset import ChestXray
+from lib.metrics import multi_label_auroc
 
 
 class Tester:
-    # TODO
-    pass
+
+    def __init__(self,
+                 model_path: str or list[str],
+                 test_set: ChestXray,
+                 metrics: list[str],
+                 threshold: float = 0.5):
+
+        self.model: list[nn.Module] = []
+
+    @staticmethod
+    def load_model(model_path: str) -> nn.Module:
+        return torch.load(model_path)
+
+    def add_model(self, model_path):
+        assert os.path.isfile(model_path), "Invalid model_path given!"
+        self.model.append(Tester.load_model(model_path))
+
