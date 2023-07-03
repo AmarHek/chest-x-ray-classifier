02cbe83f5ca8cc818a35a69cf2616ce0c52ec158
Amar <amar.hekalo@uni-wuerzburg.de>
2023-07-03 14:55:35 +0200

    test script to fix tester

--- test/test_tester.py
+++ test/test_tester.py
@@ -3,21 +3,25 @@ from src.makers.tester import Tester
 
 if __name__ == "__main__":
 
-    model_file = "/home/ls6/hekalo/models.txt"
+    # model_file = "/home/ls6/hekalo/models.txt"
+    # csv_path = "/scratch/hekalo/Datasets/CheXpert-v1.0-small/"
+    # img_path = "/scratch/hekalo/Datasets/"
 
-    csv_path = "/scratch/hekalo/Datasets/CheXpert-v1.0-small/"
-    img_path = "/scratch/hekalo/Datasets/"
-    image_size = 50
+    model_file = "C:/Users/Amar/Documents/models.txt"
+    csv_path = "C:/Users/Amar/Documents/CheXpert-v1.0-small/test_small.csv"
+    img_path = "C:/Users/Amar/Documents/"
+
+    image_size = 100
 
     labels = ["Enlarged Cardiomediastinum", "Cardiomegaly", "Lung Opacity", "Lung Lesion", "Edema",
               "Consolidation", "Pneumonia", "Atelectasis", "Pneumothorax", "Pleural Effusion",
               "Pleural Other", "Fracture", "Support Devices"]
 
-    testSet = CheXpert(csv_path=csv_path + 'test.csv', image_root_path=img_path, use_upsampling=False,
+    testSet = CheXpert(csv_path=csv_path, image_root_path=img_path, use_upsampling=False,
                        image_size=image_size, mode='test', train_cols=labels)
 
-    tester = Tester(test_set=testSet, classes=labels, models_file=model_file, batch_size=1)
+    tester = Tester(test_set=testSet, classes=labels, models_file=model_file, batch_size=1, metrics=["f1"])
 
     tester.test()
-    tester.save_metrics("/home/ls6/hekalo/metrics.json")
-    tester.save_raw_results("/home/ls6/hekalo/output.json")
+    tester.save_metrics("C:/Users/Amar/Documents/metrics.json")
+    tester.save_raw_results("C:/Users/Amar/Documents/output.json")
