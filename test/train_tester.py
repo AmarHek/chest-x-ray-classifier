916383ea2e957edd873aa9038330ff7ffb239043
Amar <amar.hekalo@uni-wuerzburg.de>
2023-07-03 15:59:32 +0200

    moved to test module

--- test/train_tester.py
+++ test/train_tester.py
@@ -1,13 +1,14 @@
 from src.datasets.chexpert import CheXpert
-from src.models import Trainer
+from src.models import cnn_models
+from src.makers.trainer import Trainer
 
 if __name__ == '__main__':
 
     csv_path = 'C:/Users/Amar/Documents/CheXpert-v1.0-small/'
     img_path = 'C:/Users/Amar/Documents/'
 
-    #classes = ["Enlarged Cardiomediastinum","Cardiomegaly","Lung Opacity","Lung Lesion","Edema","Consolidation",
-    #           "Pneumonia","Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other","Fracture","Support Devices"]
+    # classes = ["Enlarged Cardiomediastinum","Cardiomegaly","Lung Opacity","Lung Lesion","Edema","Consolidation",
+    #          "Pneumonia","Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other","Fracture","Support Devices"]
 
     classes = ["Pneumonia"]
 
@@ -16,7 +17,7 @@ if __name__ == '__main__':
     valSet = CheXpert(csv_path=csv_path + 'valid.csv', image_root_path=img_path, use_upsampling=False,
                       use_frontal=True, image_size=320, mode='valid', train_cols=classes)
 
-    model = models.sequential_model("densenet121", trainSet.num_classes)
+    model = cnn_models.sequential_model("densenet121", trainSet.num_classes)
 
     trainer = Trainer(model=model,
                       train_set=trainSet,
