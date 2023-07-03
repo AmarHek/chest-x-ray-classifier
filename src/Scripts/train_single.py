0c0d0e4039163801f5785e5563caa813ccef0d91
Amar <amar.hekalo@uni-wuerzburg.de>
2023-07-03 16:44:27 +0200

    refactoring + implemented options for other schedulers

--- src/Scripts/train_single.py
+++ src/Scripts/train_single.py
@@ -30,7 +30,13 @@ if __name__ == '__main__':
 
     parser.add_argument("--update_steps", type=int, default=1000)
     parser.add_argument("--es_patience", type=int, default=10)
+
+    # scheduler options
     parser.add_argument("--lr_scheduler", type=str, default="plateau")
+    parser.add_argument("--plateau_patience", type=int, default=5)
+    parser.add_argument("--exponential_gamma", type=float, default=0.01)
+    parser.add_argument("--cyclic_lr_min", type=float, default=0.001)
+    parser.add_argument("--cyclic_lr_max", type=float, default=0.01)
 
     args = parser.parse_args()
 
@@ -47,43 +53,28 @@ if __name__ == '__main__':
     assert args.model_path is not None, "Specify model path!"
     assert args.classes in classes_dict.keys(), "Invalid class config!"
 
-    architecture = args.architecture
     classes = classes_dict[args.classes]
 
-    model_path = args.model_path
-    csv_path = args.csv_path
-    img_path = args.img_path
-    image_size = args.image_size
+    trainSet = CheXpert(csv_path=args.csv_path + 'train.csv', image_root_path=args.img_path, use_upsampling=False,
+                        use_frontal=True, image_size=args.image_size, mode='train', train_cols=classes)
+    valSet = CheXpert(csv_path=args.csv_path + 'valid.csv', image_root_path=args.img_path, use_upsampling=False,
+                      use_frontal=True, image_size=args.image_size, mode='valid', train_cols=classes)
 
-    loss = args.loss
-    optimizer = args.optimizer
-    learning_rate = args.learning_rate
-
-    batch_size = args.batch_size
-    epochs = args.epochs
-
-    update_steps = args.update_steps
-    es_patience = args.es_patience
-    lr_scheduler = args.lr_scheduler
-
-    trainSet = CheXpert(csv_path=csv_path + 'train.csv', image_root_path=img_path, use_upsampling=False,
-                        use_frontal=True, image_size=image_size, mode='train', train_cols=classes)
-    valSet = CheXpert(csv_path=csv_path + 'valid.csv', image_root_path=img_path, use_upsampling=False,
-                      use_frontal=True, image_size=image_size, mode='valid', train_cols=classes)
-
-    model = models.sequential_model(architecture, trainSet.num_classes)
+    model = models.sequential_model(args.architecture, trainSet.num_classes)
 
     trainer = Trainer(model=model,
                       train_set=trainSet,
                       valid_set=valSet,
-                      loss=loss,
-                      optimizer=optimizer,
-                      learning_rate=learning_rate,
-                      epochs=epochs,
-                      batch_size=batch_size,
-                      update_steps=update_steps,
-                      early_stopping_patience=es_patience,
-                      lr_scheduler=lr_scheduler,
-                      plateau_patience=5)
+                      loss=args.loss,
+                      optimizer=args.optimizer,
+                      learning_rate=args.learning_rate,
+                      epochs=args.epochs,
+                      batch_size=args.batch_size,
+                      update_steps=args.update_steps,
+                      early_stopping_patience=args.es_patience,
+                      lr_scheduler=args.lr_scheduler,
+                      plateau_patience=args.plateau_patience,
+                      exponential_gamma=args.exponential_gamma,
+                      cyclic_lr=(args.cyclic_lr_min, args.cyclic_lr_max))
 
-    trainer.train(model_path, architecture)
+    trainer.train(args.model_path, args.architecture)
