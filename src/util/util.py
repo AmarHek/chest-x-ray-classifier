110b526b21d2b1a43b5e0aca10c4a2d11d4d33c1
Amar <amar.hekalo@uni-wuerzburg.de>
2023-07-03 14:53:45 +0200

    added helper method to serialize array of arrays into json serializable format

--- src/util/util.py
+++ src/util/util.py
@@ -2,3 +2,12 @@ def get_file_contents_as_list(filename):
     with open(filename, 'r') as file:
         lines = [line.rstrip() for line in file]
     return lines
+
+
+def serialize_numpy_array(numpy_array):
+    serialized_dict = {}
+
+    for idx, output in enumerate(numpy_array):
+        serialized_dict[idx] = output.tolist()
+
+    return serialized_dict
