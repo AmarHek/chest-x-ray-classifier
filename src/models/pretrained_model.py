b2a0e2a7acdf29162f04b12d65cec2b32fc34445
AmarHek <amar.hekalo@uni-wuerzburg.de>
2023-11-30 17:28:24 +0100

    pretrained_model.py now fully implemented

--- src/models/pretrained_model.py
+++ src/models/pretrained_model.py
@@ -1,38 +1,59 @@
 import torch
 from torch import nn
-from torch.nn import Sequential
 
-from params.model_params import ModelParams
-from models.model_components import get_backbone
+from params import PretrainedModelParams
+from models import get_backbone, freeze_layers, classifier_head, classifier_function
 
 
 class PretrainedModel(nn.Module):
 
-    def __init__(self, model_params: ModelParams):
+    def __init__(self, model_params: PretrainedModelParams):
 
         super(PretrainedModel, self).__init__()
         self.model_params = model_params
 
         self.backbone = get_backbone(self.model_params.backbone, self.model_params.weights)
+        freeze_layers(self.backbone, self.model_params.freeze_depth)
 
-        self.classifier = self.get_classifier()
+        num_features = self.get_num_features(self.backbone)
+
+        avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        if self.model_params.dropout > 0:
+            dropout = nn.Dropout(self.model_params.dropout)
+        else:
+            dropout = nn.Identity()
+
+        head = classifier_head(in_features=num_features, **self.model_params.to_dict())
+        cls_function = classifier_function(self.model_params.classifier_function)
+        self.classifier = nn.Sequential(avg_pool, dropout, head, cls_function)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.classifier(x)
+        return x
 
     @staticmethod
-    def get_num_features(model, input_tensor):
-        # Extract the feature part of the model up to the final pooling layer
-        features = Sequential(*list(model.children())[:-1])
-
-        # Put the feature part and a dummy input through the model to get the output size
+    def get_num_features(feature_extractor: nn.Module) -> int:
+        dummy_input = torch.zeros(1, 3, 224, 224)
+        # Run a forward pass of the dummy input tensor through the feature extractor
         with torch.no_grad():
-            output = features(input_tensor)
-
-        # Return the number of output features
-        return output.view(output.size(0), -1).shape[1]
+            output = feature_extractor(dummy_input)
+        # Return the channel dimension of the output
+        return output.shape[1]
 
 
 if __name__ == "__main__":
-    import torchvision.models as tvmodels
+    dummy_input = torch.zeros(4, 3, 224, 224)
 
-    weights = tvmodels.ViT_L_32_Weights.DEFAULT
-    preprocess = weights.transforms
-    print(preprocess)
+    model_params = PretrainedModelParams()
+    model_params.backbone = "efficientnet_b0"
+    model_params.head = "csra"
+    model_params.num_heads = 4
+    model_params.lam = 0.1
+    model_params.num_classes = 14
+
+    model = PretrainedModel(model_params)
+    print(model)
+
+    print(model(dummy_input).shape)
