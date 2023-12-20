b4b515353e0aa57ba0a799343d9fa2dcd36157de
AmarHek <amar.hekalo@uni-wuerzburg.de>
2023-12-20 17:47:38 +0100

    update requirements and setup

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='Chest X-Ray Classifier',
-    version='0.2',
-    packages=find_packages()
+    version='1.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='amar',
+    author_email='amar.hekalo@uni-wuerzburg.de'
 )
