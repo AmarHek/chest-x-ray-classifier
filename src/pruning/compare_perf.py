aa06638528a0d72f837725f067767745cf2d1ab7
Finn <finn.jaeger@stud-mail.uni-wuerzburg.de>
2024-03-30 20:15:02 +0100

    added additional difficulty measures

--- src/pruning/compare_perf.py
+++ src/pruning/compare_perf.py
@@ -21,7 +21,21 @@ paths = yaml_data['compParams']
 #print("Argument 1:", args.arg1)
 #print("Argument 2:", args.arg2)
 
-
+def get_change_in_prob_row(l1,l2):
+    sum = 0
+    for i in range(len(l1)):
+        sum+=abs(l1[i]-l2[i])
+    return sum
+def get_change_in_prob_overall(df,no,dfs):
+    l1cols = [f"{i}_{dfs[0]}" for i in range(no)]
+    l2cols = [f"{i}_{dfs[0]}" for i in range(no)]
+    res = []
+    for index, row in df.iterrows():
+        l1 = df.loc[index,:][l1cols].tolist()
+        l2 = df.loc[index,:][l2cols].tolist()
+        
+        res.append(get_change_in_prob_row(l1,l2))
+    return res
 def compare_performances(path_normal,
                         path_pruned,
                         path_ground_truth,
@@ -34,12 +48,19 @@ def compare_performances(path_normal,
         df1 = pd.read_csv(f"{path_normal}/chexpert/outputs.csv")
         df2 = pd.read_csv(f"{path_pruned}/chexpert/outputs.csv")
 
+    illness = yaml_data['compParams']['illness']
+    no_illness = len(illness)
+
     tdf = pd.read_csv(path_ground_truth)
     # Concatenate based on 'filename'
     result = pd.merge(df1, df2, on='filename', how='outer')
     columns_to_transform = result.columns.drop("filename").tolist()
+
+    #Difficulty measure 1: calculate pruning impact as difference in probabilities
+    result[f'PI_{pruning_method}_prob'] = get_change_in_prob_overall(result,no_illness,['x','y'])
     #convert output to boolean values
     result.loc[:, columns_to_transform] = result.loc[:, columns_to_transform].map(lambda x: 1 if x >= 0.5 else 0)
+    result[f'PI_{pruning_method}_hamming'] = get_change_in_prob_overall(result,no_illness,['x','y'])
     #shorten filenames to match format of tdf
     result.filename = result.filename.map(lambda x: x[x.find("CheXpert-v1.0-small"):])
 
@@ -48,23 +69,25 @@ def compare_performances(path_normal,
 
     #get performance for both dfs for each illness
     #illness = tdf.columns.tolist()[6:]
-    illness = ['Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
-    for i in range(13):
+    #illness = ['Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
+    
+    for i in range(no_illness):
         for df in ['x','y']:
             result[f"perf_{df}_{i}"] = (result[f'{i}_{df}'] == result[illness[i]]).astype(int)
 
             #get overall performance
-            if i == 12:
-                result[f"perf_{df}_total"] = result[[f"perf_{df}_{k}" for k in range(13)]].sum(axis=1)
+            if i == no_illness-1:
+                result[f"perf_{df}_total"] = result[[f"perf_{df}_{k}" for k in range(no_illness)]].sum(axis=1)
 
     #get pruning impact
-    result[f'PI_{pruning_method}'] = result[f"perf_x_total"] - result[f"perf_y_total"]
+    result[f'PI_{pruning_method}_acc'] = result[f"perf_x_total"] - result[f"perf_y_total"]
     print('succesfully inferred pruning impact')
     
     #add pruning impact to difficulties file
 
     ddf = pd.read_csv(export_result)
-    exp_df = pd.merge(ddf,result[['filename',f'PI_{pruning_method}']],left_on = 'Path',right_on='filename',how="outer")
+    #3. difficult measure: difference in accuracy
+    exp_df = pd.merge(ddf,result[['filename',f'PI_{pruning_method}_prob',f'PI_{pruning_method}_acc',f'PI_{pruning_method}_hamming']],left_on = 'Path',right_on='filename',how="outer")
     #
     cols_to_drop = []
     for c in exp_df.columns: 
